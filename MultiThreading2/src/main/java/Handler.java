d4f72d7cdd081566b5164f12f36ff87b0d12e348
Vadim Basalaev <basalaev.vad@yandex.ru>
2022-12-04 22:44:23 +0400

    Target*Request

---
+++ MultiThreading2/src/main/java/Handler.java
@@ -0,0 +1,8 @@
+import java.io.BufferedOutputStream;
+
+@FunctionalInterface
+public interface Handler {
+
+    void handle(Request request, BufferedOutputStream responseStream);
+
+}
---
+++ MultiThreading2/src/main/java/Request.java
@@ -0,0 +1,36 @@
+import java.io.InputStream;
+import java.util.Map;
+
+public class Request {
+
+    private final String method;
+    private final String path;
+    private final Map<String, String> headers;
+    private final InputStream body;
+
+    public Request(String method, String path, Map<String, String> headers, InputStream body) {
+        this.method = method;
+        this.path = path;
+        this.headers = headers;
+        this.body = body;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "method='" + method + '\'' +
+                ", path='" + path + '\'' +
+                ", headers=" + headers +
+                ", body='" + body + '\'' +
+                '}';
+
+    }
+}
